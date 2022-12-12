e61332cd4eab9767430087517d4cc1ba8de88aca
Aabhimanyu Gupta <abhimanyu10gupta@gmail.com>
2022-12-12 21:03:37 +1000

    change imports

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DATABASE_URL="postgresql://ibbbxqavtnflqj:905ab0a9708c5bbdb6a5a71721d01447a47903c4cada2f1bb6f667b3c85527b2@ec2-44-199-143-43.compute-1.amazonaws.com:5432/debm04m4a0kdb0"
+export DATABASE_URL="postgresql://postgres:asdasd@localhost:5432/agency"
 export AUTH0_DOMAIN="fullstack-udacity.au.auth0.com"
 export AUTH0_CLIENTID="UIJR3QLTuDJiObMrS7dX66OHtFodc4BF"
 export EXCITED="true"
